5ee1c5654233d5a94703066715218fb7482d37ec
piquu <l.smole@protonmail.com>
2026-03-23 18:43:58 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  outputs =
+    {
+      self,
+    }:
+    {
+      lib =
+        { nixpkgs }:
+        with nixpkgs.lib;
+        rec {
+          pipe = {
+            inherit mkTf;
+            terraform = attrs: pipe: pipe ++ [ (terraform attrs) ];
+            provider = attrs: pipe: pipe ++ [ (provider attrs) ];
+            variable =
+              name: attrs: pipe:
+              pipe ++ [ (variable name attrs) ];
+            resource =
+              type: name: attrs: pipe:
+              pipe ++ [ (resource type name attrs) ];
+            output =
+              name: attrs: pipe:
+              pipe ++ [ (output name attrs) ];
+          };
+
+          terraform = attrs: {
+            terraform = attrs;
+          };
+
+          provider = attrs: {
+            provider = attrs;
+          };
+
+          variable = name: attrs: {
+            variable.${name} = attrs;
+          };
+
+          resource = type: name: attrs: {
+            resources.${type}.${name} = attrs;
+          };
+
+          output = name: attrs: {
+            output.${name} = attrs;
+          };
+
+          mkTf = list: toJSON (foldl' recursiveUpdate { } list);
+        };
+    };
+}
